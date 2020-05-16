a969f78c2b3d22c9051588c81482dd2c95c36eba
tgmeng <tgmeng@163.com>
2020-05-16 08:01:59 +0800

    refactor: add channel max_user

--- lib/show_me_the_code_web/channels/room_channel.ex
+++ lib/show_me_the_code_web/channels/room_channel.ex
@@ -2,16 +2,22 @@ defmodule ShowMeTheCodeWeb.RoomChannel do
   use Phoenix.Channel
   alias ShowMeTheCodeWeb.Presence
 
+  @max_user 5
+
   defp valid_room_id?(room_id) do
     room_id =~ ~r/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i
   end
 
   def join("room:" <> room_id, %{"user_name" => user_name}, socket) when not is_nil(user_name) do
     if valid_room_id?(room_id) do
-      send(self(), :after_join)
+      if map_size(Presence.list(socket)) + 1 > @max_user do
+        {:error, %{"msg" => "room is full"}}
+      else
+        send(self(), :after_join)
 
-      {:ok, %{"user_name" => user_name, user_id: socket.assigns.user_id},
-       socket |> assign(:room_id, room_id) |> assign(:user_name, user_name)}
+        {:ok, %{"user_name" => user_name, user_id: socket.assigns.user_id},
+         socket |> assign(:room_id, room_id) |> assign(:user_name, user_name)}
+      end
     else
       {:error, %{"msg" => "room id is invalid"}}
     end
